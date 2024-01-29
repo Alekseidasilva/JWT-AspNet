32e0f0b212e6b392764161ae52d05e32cce0369c
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-01-30 00:43:08 +0100

    FEAT:  ValueObject de Email

--- JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -4,10 +4,32 @@ using JwtStore.Core.SharedContext.ValueObjects;
 
 namespace JwtStore.Core.AccountContext.ValueObjects;
 
-public class Email:ValueObject
+public partial class Email:ValueObject
 {
     private const string Pattern = @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
     
     public string Address { get; }
     public string Hash => Address.ToBase64();
+
+
+    public static implicit operator string(Email email) => email.ToString();
+    public static implicit operator Email(string address) =>new Email(address);
+    public override string ToString() => Address;
+
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex EmailRegex();
+
+    public Email(string address)
+    {
+        if (string.IsNullOrEmpty(address))
+            throw new Exception("Email Inválido");
+        
+        address = address.Trim().ToLower();
+        
+        if (address.Length < 5)
+            throw new Exception("Email Inválido");
+        if (!EmailRegex().IsMatch(address))
+            throw new Exception("Email Inválido");
+    }
 }
