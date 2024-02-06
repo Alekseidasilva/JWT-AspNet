1e690fa5f5ae13392c6850de9f844f2cdf3205e7
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-02-06 21:27:33 +0100

    FEAT: Registrando Dependencias

--- JwtStore/JwtStore.api/Extension/AccountContextExtension.cs
+++ JwtStore/JwtStore.api/Extension/AccountContextExtension.cs
@@ -4,7 +4,16 @@ public static class AccountContextExtension
 {
     public static void AddAccountContext(this WebApplicationBuilder builder)
     {
+        #region Create
 
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Repository>();
+
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IService,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>();
+        #endregion
     }
 
     public static void MapAccountEndpoints(this WebApplication app)
